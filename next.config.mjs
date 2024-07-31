ed701a9300a2fda770dbe5ced4fd3a5f430afd19
DamEspinola <britezespinola19@gmail.com>
2024-07-31 17:01:15 -0400

    changes in the nextconfig and add placeholder

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,16 @@
 const nextConfig = {
     eslint: {
         dirs: ['pages', 'components', 'lib', 'src']
-      }
+      },
+      images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'res.cloudinary.com',
+          },
+        ],
+      },
+
 };
 
 export default nextConfig;
